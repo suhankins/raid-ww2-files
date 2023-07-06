17d79e9b44700ee44b3b27c170ac18aae828e33b
suhankins <punishedbernadetta@niuitmo.ru>
2023-07-06 11:32:32 +0400

    made getSteamStats throw an error if request fails

--- src/utils/getSteamStats/getSteamStats.ts
+++ src/utils/getSteamStats/getSteamStats.ts
@@ -1,9 +1,17 @@
+import { ISteamStats } from '@/lib/ISteamStats';
 import { steamStatsArrayToObject } from '../steamStatsArrayToObject';
 
-export async function getSteamStats(steamid: string | number) {
+/**
+ * Gets steam stats for given steamid.
+ * @throws If can't get steam stats
+ */
+export async function getSteamStats(
+    steamid: string | number
+): Promise<ISteamStats> {
     const response = await fetch(
         `http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=414740&key=${process.env.STEAM_WEB_API_KEY}&steamid=${steamid}&format=json`
     );
+    if (!response.ok) throw new Error("Can't get steam stats");
     const data = await response.json();
     return steamStatsArrayToObject(data.playerstats.stats);
 }
