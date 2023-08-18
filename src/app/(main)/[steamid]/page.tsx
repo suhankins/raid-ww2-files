06e941f5d84e7eda403e74b90f6524755539c0e4
suhankins <punishedbernadetta@niuitmo.ru>
2023-08-18 16:08:27 +0400

    moved searchbar to layout to allow main page to work without steam id

--- src/app/(main)/[steamid]/page.tsx
+++ src/app/(main)/[steamid]/page.tsx
@@ -1,7 +1,9 @@
+import { PlayerCard } from '@/components/PlayerCard/PlayerCard';
+import { getAchievements } from '@/utils/getAchievements/getAchievements';
 import { getGrenadeIdFromIndex } from '@/utils/getFromIndex/getGrenadeIdFromIndex';
 import { getMeleeIdFromIndex } from '@/utils/getFromIndex/getMeleeIdFromIndex';
 import { getWeaponIdFromIndex } from '@/utils/getFromIndex/getWeaponIdFromIndex';
-import { getSteamStats } from '@/utils/getSteamStats/getSteamStats';
+import { getStats } from '@/utils/getStats/getStats';
 import { getTagline } from '@/utils/getTagline';
 import { getUserInfo } from '@/utils/getUserInfo/getUserInfo';
 
@@ -10,21 +12,29 @@ export default async function Home({
 }: {
     params: { steamid: string };
 }) {
+    if (!steamid.match(/^[0-9]+$/)) {
+        // Instead of SteamID we were probably given a vanity URL
+        const givenUrl = decodeURIComponent(steamid);
+        const vanityUrl = givenUrl.match(
+            /(?<=https:\/\/steamcommunity\.com\/id\/)(\w+)/gm
+        );
+        if (vanityUrl) {
+            // TODO: call resolveVanityUrl with vanityUrl[0]
+        }
+    }
     // My steamid: 76561198061159261
     const user = await getUserInfo(steamid);
-    const stats = await getSteamStats(steamid);
+    const stats = await getStats(steamid);
+    await getAchievements(steamid);
     return (
         <>
-            <code>
-                {user.personaname}
-                {user.avatarfull}
-                {user.profileurl}
-                {getTagline(stats)}
+            <div>
+                <PlayerCard {...user} tagline={getTagline(stats)} />
                 {getWeaponIdFromIndex(stats.equipped_primary)}
                 {getWeaponIdFromIndex(stats.equipped_secondary)}
                 {getGrenadeIdFromIndex(stats.equipped_grenade)}
                 {getMeleeIdFromIndex(stats.equipped_melee)}
-            </code>
+            </div>
             <div>{JSON.stringify(stats)}</div>
         </>
     );
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,12 +1,32 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { FormEvent, useRef } from 'react';
+
 export default function MainLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const router = useRouter();
+    const inputRef = useRef<HTMLInputElement>(null);
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        if (inputRef.current) {
+            const value = inputRef.current.value;
+            if (value) {
+                router.push(`./${encodeURIComponent(value)}`);
+            }
+        }
+    };
     return (
         <main>
-            <form>
-                <input type="text" placeholder="Steam ID" />
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Steam ID or URL"
+                    ref={inputRef}
+                />
                 <button>Submit</button>
             </form>
             {children}
