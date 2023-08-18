9dc5bd7cb071b3c5ba9a15ebb1ce3e983aa2ef6d
suhankins <punishedbernadetta@niuitmo.ru>
2023-08-18 23:16:04 +0400

    added less placeholder-y taglines

--- src/utils/getTagline.ts
+++ src/utils/getTagline.ts
@@ -10,52 +10,52 @@ function fullfilmentLevelFactory(level: number) {
 
 /**
  * All possible taglines. Sorted by priority, so the first one that matches is the one that is used.
- * TODO: Write good taglines, current ones are placeholders
+ * TODO: Write good taglines, current ones could be improved.
  */
 const taglines = [
     {
         fullfilment: fullfilmentLevel40CountFactory(5),
-        tagline: 'Level 40 x5',
+        tagline: 'Destructive force of nature',
     },
     {
         fullfilment: fullfilmentLevel40CountFactory(4),
-        tagline: 'Level 40 x4',
+        tagline: 'Unstoppable force',
     },
     {
         fullfilment: fullfilmentLevel40CountFactory(3),
-        tagline: 'Level 40 x3',
+        tagline: 'Extremely dangerous',
     },
     {
         fullfilment: fullfilmentLevel40CountFactory(2),
-        tagline: 'Level 40 x2',
+        tagline: 'Terrifyingly effecient agent',
     },
     {
         fullfilment: fullfilmentLevelFactory(40),
-        tagline: 'Level 40',
+        tagline: 'Professional saboteur',
     },
     {
         fullfilment: fullfilmentLevelFactory(30),
-        tagline: 'Level 30',
+        tagline: 'Experienced operative',
     },
     {
         fullfilment: fullfilmentLevelFactory(20),
-        tagline: 'Level 20',
+        tagline: 'Competent agent',
     },
     {
         fullfilment: fullfilmentLevelFactory(10),
-        tagline: 'Level 10',
+        tagline: 'Battle-tested recruit',
     },
     {
         fullfilment: fullfilmentLevelFactory(5),
-        tagline: 'Level 5',
+        tagline: 'Slightly above average shooter',
     },
     {
         fullfilment: fullfilmentLevelFactory(2),
-        tagline: 'Level 2',
+        tagline: '"Intern" raider',
     },
     {
         fullfilment: () => true,
-        tagline: 'Level 1',
+        tagline: 'Just recruited',
     },
 ];
 
