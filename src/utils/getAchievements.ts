475ff4afe1b8e01f05d2a1332fca9cf844463ab0
suhankins <punishedbernadetta@niuitmo.ru>
2023-08-18 23:15:45 +0400

    updated getAchievements util

--- src/utils/getAchievements.ts
+++ src/utils/getAchievements.ts
@@ -1,8 +1,18 @@
+export interface Achievement {
+    apiname: string;
+    achieved: number;
+    unlocktime: number;
+    name: string;
+    description: string;
+}
+
 /**
  * Gets steam achievements for given steamid.
  * @throws If can't get steam stats
  */
-export async function getAchievements(steamid: string | number) {
+export async function getAchievements(
+    steamid: string | number
+): Promise<Achievement[]> {
     // we use v0001 api because v0002 doesn't return unlocktime
     const response = await fetch(
         `http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=414740&key=${process.env.STEAM_WEB_API_KEY}&steamid=${steamid}&l=en`,
@@ -10,5 +20,7 @@ export async function getAchievements(steamid: string | number) {
     );
     if (!response.ok) throw new Error("Can't get steam achievements");
     const data = await response.json();
-    //console.log(data.playerstats.achievements);
+    if (!data.playerstats || !data.playerstats.achievements)
+        throw new Error("Can't get steam achievements");
+    return data.playerstats.achievements;
 }
