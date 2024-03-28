eb16a274e27e531ce3e2898ee93b78dd22c3b57d
suhankins <punishedbernadetta@niuitmo.ru>
2024-03-28 13:25:24 +0400

    figured out what happens when player has coin equipped

--- src/utils/getFromIndex/getGrenadeIdFromIndex.ts
+++ src/utils/getFromIndex/getGrenadeIdFromIndex.ts
@@ -1,6 +1,8 @@
 // https://github.com/steam-test1/Raid-WW2-Lua-Complete/blob/c593f4b5b08f2a1ee8e72e0c8a7f50514e89a0a2/lib/tweak_data/statisticstweakdata.lua#L235
 
-// TODO: Figure out what happens if user equips a coin
+/**
+ * If player has coin equipped, steam stat won't be updated
+ */
 const grenadeList = ['m24', 'concrete', 'd343', 'mills', 'betty'];
 
 /**
