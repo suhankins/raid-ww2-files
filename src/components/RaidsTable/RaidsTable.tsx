93a34b7b64f27325d6928b27aefc7680c17c1fe7
suhankins <punishedbernadetta@niuitmo.ru>
2024-05-16 13:48:04 +0400

    added Display operation days separately checkbox

--- src/components/RaidsTable/RaidsTable.tsx
+++ src/components/RaidsTable/RaidsTable.tsx
@@ -3,7 +3,7 @@
 
 import { type ISteamStats } from '@/lib/ISteamStats';
 import styles from './RaidsTable.module.css';
-import { Jobs } from '@/utils/RaidDB';
+import { Jobs, RaidsAndDays } from '@/utils/RaidDB';
 import getJobStat from '@/utils/getJobStat';
 import { useMemo, useState } from 'react';
 import Stepper from '../Stepper/Stepper';
@@ -13,6 +13,7 @@ import formatJobStat from '@/utils/formatJobStat';
 import getJobAccomplishments from '@/utils/getJobAccomplishments';
 import { type IAchievement } from '@/lib/IAchievement';
 import getAccomplishmentString from '@/utils/getAccomplishmentString';
+import Checkbox from '../Checkbox/Checkbox';
 
 export default function RaidsTable({
     stats,
@@ -22,38 +23,52 @@ export default function RaidsTable({
     achievements: IAchievement[];
 }) {
     const [selectedStat, setSelectedStat] = useState<IRaidStat>('Completions');
+    const [operationDaysSeparate, setOperationDaysSeparate] = useState(false);
+
+    const listOfJobs = useMemo(
+        () => (operationDaysSeparate ? RaidsAndDays : Jobs),
+        [operationDaysSeparate]
+    );
 
     const total = useMemo(
         () =>
-            Jobs.reduce(
+            listOfJobs.reduce(
                 (total, job) => total + getJobStat(job, selectedStat, stats),
                 0
             ),
-        [selectedStat, stats]
+        [selectedStat, stats, listOfJobs]
     );
 
     const highest = useMemo(
         () =>
-            Jobs.reduce(
+            listOfJobs.reduce(
                 (highest, job) =>
                     Math.max(highest, getJobStat(job, selectedStat, stats)),
                 0
             ),
-        [selectedStat, stats]
+        [selectedStat, stats, listOfJobs]
     );
 
     return (
         <section>
-            <Stepper
-                id="raidStat"
-                options={['Completions', 'Starts', 'Time']}
-                selectedOption={selectedStat}
-                onChange={(value) =>
-                    setSelectedStat(value as 'Completions' | 'Time')
-                }
-            >
-                Sort by
-            </Stepper>
+            <div className="controls">
+                <Stepper
+                    id="raidStat"
+                    options={['Completions', 'Starts', 'Time']}
+                    selectedOption={selectedStat}
+                    onChange={(value) =>
+                        setSelectedStat(value as 'Completions' | 'Time')
+                    }
+                >
+                    Sort by
+                </Stepper>
+                <Checkbox
+                    checked={operationDaysSeparate}
+                    onChange={(value) => setOperationDaysSeparate(value)}
+                >
+                    Display operation days separately
+                </Checkbox>
+            </div>
             <table className={styles.table}>
                 <thead>
                     <tr>
@@ -64,61 +79,73 @@ export default function RaidsTable({
                     </tr>
                 </thead>
                 <tbody>
-                    {Jobs.sort(
-                        (raid1, raid2) =>
-                            getJobStat(raid2, selectedStat, stats) -
-                            getJobStat(raid1, selectedStat, stats)
-                    ).map((raid) => {
-                        const jobStat = getJobStat(raid, selectedStat, stats);
-                        const percentageOfTotal = toPercentage(jobStat / total);
-                        const percentageOfMax = toPercentage(jobStat / highest);
+                    {listOfJobs
+                        .sort(
+                            (raid1, raid2) =>
+                                getJobStat(raid2, selectedStat, stats) -
+                                getJobStat(raid1, selectedStat, stats)
+                        )
+                        .map((raid) => {
+                            const jobStat = getJobStat(
+                                raid,
+                                selectedStat,
+                                stats
+                            );
+                            const percentageOfTotal = toPercentage(
+                                jobStat / total
+                            );
+                            const percentageOfMax = toPercentage(
+                                jobStat / highest
+                            );
 
-                        return (
-                            <tr key={raid.name}>
-                                <td>
-                                    <img
-                                        alt=""
-                                        src={`/static/images/raid/raids/${raid.type === 'raid' && raid.parent ? raid.parent : raid.id}.png`}
-                                    />
-                                </td>
-                                <td>{raid.name}</td>
-                                <td>
-                                    {getJobAccomplishments(
-                                        raid,
-                                        achievements
-                                    ).map((accomplishment) => (
+                            return (
+                                <tr key={raid.name}>
+                                    <td>
                                         <img
-                                            key={accomplishment.type}
-                                            src={`/static/images/raid/accomplishments/${accomplishment.type}.png`}
-                                            className={styles.accomplishment}
-                                            data-completed={
-                                                accomplishment.completed
-                                            }
-                                            title={getAccomplishmentString(
-                                                accomplishment
-                                            )}
-                                            alt={getAccomplishmentString(
-                                                accomplishment
-                                            )}
+                                            alt=""
+                                            src={`/static/images/raid/raids/${raid.type === 'raid' && raid.parent ? raid.parent : raid.id}.png`}
                                         />
-                                    ))}
-                                </td>
-                                <td title={percentageOfTotal}>
-                                    <div
-                                        className={styles.bar}
-                                        style={{
-                                            width: percentageOfMax,
-                                        }}
-                                    >
-                                        {formatJobStat(
-                                            jobStat,
-                                            percentageOfTotal
-                                        )}
-                                    </div>
-                                </td>
-                            </tr>
-                        );
-                    })}
+                                    </td>
+                                    <td>{raid.name}</td>
+                                    <td>
+                                        {getJobAccomplishments(
+                                            raid,
+                                            achievements
+                                        ).map((accomplishment) => (
+                                            <img
+                                                key={accomplishment.type}
+                                                src={`/static/images/raid/accomplishments/${accomplishment.type}.png`}
+                                                className={
+                                                    styles.accomplishment
+                                                }
+                                                data-completed={
+                                                    accomplishment.completed
+                                                }
+                                                title={getAccomplishmentString(
+                                                    accomplishment
+                                                )}
+                                                alt={getAccomplishmentString(
+                                                    accomplishment
+                                                )}
+                                            />
+                                        ))}
+                                    </td>
+                                    <td title={percentageOfTotal}>
+                                        <div
+                                            className={styles.bar}
+                                            style={{
+                                                width: percentageOfMax,
+                                            }}
+                                        >
+                                            {formatJobStat(
+                                                jobStat,
+                                                percentageOfTotal
+                                            )}
+                                        </div>
+                                    </td>
+                                </tr>
+                            );
+                        })}
                 </tbody>
             </table>
         </section>
