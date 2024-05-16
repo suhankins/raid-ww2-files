058e8eca0f8a19afaae21482cd766ac3913dd737
suhankins <punishedbernadetta@niuitmo.ru>
2024-05-16 21:09:21 +0400

    changed error card wording

--- src/app/(main)/[steamid]/ErrorCard.tsx
+++ src/app/(main)/[steamid]/ErrorCard.tsx
@@ -5,8 +5,8 @@ export default function ErrorCard({ e }: { e: unknown }) {
                 <h1>Our spies came back empty handed!</h1>
                 <p>{(e instanceof Error && e.message) || 'Unknown error'}</p>
                 <p>
-                    Check that SteamID or their profile URL is correct, or try
-                    again later.
+                    Check that SteamID or profile URL is correct, or try again
+                    later.
                 </p>
             </div>
         </main>
