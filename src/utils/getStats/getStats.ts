d41ae8d359e691c64e3b913218bbea951dedd974
suhankins <punishedbernadetta@niuitmo.ru>
2024-05-16 21:09:00 +0400

    added more helpful getStats error messages

--- src/utils/getStats/getStats.ts
+++ src/utils/getStats/getStats.ts
@@ -10,7 +10,11 @@ export async function getStats(steamid: string | number): Promise<ISteamStats> {
         `http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=414740&key=${process.env.STEAM_WEB_API_KEY}&steamid=${steamid}&format=json`,
         { cache: 'no-store' }
     );
-    if (!response.ok) throw new Error("Can't get steam stats");
+    if (!response.ok) {
+        throw new Error(
+            `Can't get steam stats: server responded ${response.status} (${response.statusText})`
+        );
+    }
     const data = await response.json();
     return steamStatsArrayToObject(data.playerstats.stats);
 }
