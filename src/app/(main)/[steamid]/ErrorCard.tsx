d6a40b31d106a8a17f2b0e60754d73c316d94be4
suhankins <punishedbernadetta@niuitmo.ru>
2024-05-21 19:09:31 +0400

    added stack to error screen

--- src/app/(main)/[steamid]/ErrorCard.tsx
+++ src/app/(main)/[steamid]/ErrorCard.tsx
@@ -1,9 +1,16 @@
 export default function ErrorCard({ e }: { e: unknown }) {
     return (
-        <main>
+        <main className="limited-width">
             <div role="alert">
                 <h1>Our spies came back empty handed!</h1>
-                <p>{(e instanceof Error && e.message) || 'Unknown error'}</p>
+                {e instanceof Error ? (
+                    <>
+                        <p>{e.message}</p>
+                        <code>{e.stack}</code>
+                    </>
+                ) : (
+                    <p>Unknown error</p>
+                )}
                 <p>
                     Check that SteamID or profile URL is correct, or try again
                     later.
