fb2518c2278e34219fbf47d0fab6211404e3b68b
suhankins <punishedbernadetta@niuitmo.ru>
2024-05-21 19:18:13 +0400

    put technical information further

--- src/app/(main)/[steamid]/ErrorCard.tsx
+++ src/app/(main)/[steamid]/ErrorCard.tsx
@@ -3,18 +3,18 @@ export default function ErrorCard({ e }: { e: unknown }) {
         <main className="limited-width">
             <div role="alert">
                 <h1>Our spies came back empty handed!</h1>
-                {e instanceof Error ? (
-                    <>
-                        <p>{e.message}</p>
-                        <code>{e.stack}</code>
-                    </>
-                ) : (
-                    <p>Unknown error</p>
-                )}
+                <p>{e instanceof Error ? e.message : 'Unknown error'}</p>
                 <p>
                     Check that SteamID or profile URL is correct, or try again
                     later.
                 </p>
+                <br />
+                {e instanceof Error && (
+                    <details>
+                        <summary>Technical information</summary>
+                        <code>{e.stack}</code>
+                    </details>
+                )}
             </div>
         </main>
     );
