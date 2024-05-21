795ca3cd592bc515a2a340297f6b1c554ae10b79
suhankins <punishedbernadetta@niuitmo.ru>
2024-05-22 01:05:25 +0400

    added unlock time to achievements

--- src/components/AchievementsList/getAchievementTooltipHtml.ts
+++ src/components/AchievementsList/getAchievementTooltipHtml.ts
@@ -1,9 +1,14 @@
-import { IAchievement } from '@/lib/IAchievement';
-import { IAchievementSchema } from '@/lib/IAchievementSchema';
+import { type IAchievement } from '@/lib/IAchievement';
+import { type IAchievementSchema } from '@/lib/IAchievementSchema';
 import styles from './AchievementList.module.css';
+import { numberToIsoDate } from '@/utils/numberToIsoDate';
 
 export default function getAchievementTooltipHtml(
     achievement: IAchievement & IAchievementSchema
 ) {
-    return `<h3 class="${styles.achievementTooltip}">${achievement.displayName}</h3><p class="${styles.achievementTooltip}">${achievement.description}</p>`;
+    return `<div class="${styles.achievementTooltip}">
+    <h3>${achievement.displayName}</h3>
+    <p>${achievement.description}</p>
+    <p>${achievement.achieved ? `Unlocked: ${numberToIsoDate(achievement.unlocktime)}` : 'Not unlocked'}</p>
+    </div>`;
 }
--- src/utils/numberToIsoDate.ts
+++ src/utils/numberToIsoDate.ts
@@ -1,4 +1,8 @@
 export function numberToIsoDate(timestamp: number) {
     const date = new Date(timestamp * 1000);
-    return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+
+    return `${year}/${month}/${day}`;
 }
