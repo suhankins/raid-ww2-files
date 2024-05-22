687c76ba254fd7d20af2ba5c1bec451bf93e827e
suhankins <punishedbernadetta@niuitmo.ru>
2024-05-22 15:02:36 +0400

    improved searchbar

---
+++ src/app/(main)/SteamIDInputForm/SteamIDInputForm.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Button from '@/components/Button/Button';
+import Textbox from '@/components/Textbox/Textbox';
+import { useRouter } from 'next/navigation';
+import { type FormEvent } from 'react';
+import styles from './SteamIDInputForm.module.css';
+
+export default function SteamIDInputForm() {
+    const router = useRouter();
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        const formData = new FormData(e.target as HTMLFormElement);
+        const steamid = formData.get('steamid');
+        if (steamid) {
+            router.push(`/${encodeURIComponent(steamid.toString())}`);
+        }
+    };
+    return (
+        <form
+            onSubmit={handleSubmit}
+            className={'limited-width ' + styles.form}
+            role="search"
+        >
+            <Textbox
+                type="search"
+                placeholder="https://steamcommunity.com/id/wolfgang"
+                name="steamid"
+            >
+                Enter Raider&apos;s Steam ID or their profile URL
+            </Textbox>
+            <Button type="submit">Search</Button>
+        </form>
+    );
+}
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,36 +1,13 @@
-'use client';
-
-import Button from '@/components/Button/Button';
-import Textbox from '@/components/Textbox/Textbox';
-import { useRouter } from 'next/navigation';
-import { type FormEvent } from 'react';
+import SteamIDInputForm from './SteamIDInputForm/SteamIDInputForm';
 
 export default function MainLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    const router = useRouter();
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
-        const formData = new FormData(e.target as HTMLFormElement);
-        const steamid = formData.get('steamid');
-        if (steamid) {
-            router.push(`./${encodeURIComponent(steamid.toString())}`);
-        }
-    };
     return (
         <div>
-            <form onSubmit={handleSubmit} className="limited-width">
-                <Textbox
-                    type="text"
-                    placeholder="https://steamcommunity.com/id/wolfgang"
-                    name="steamid"
-                >
-                    Enter Raider&apos;s Steam ID or their profile URL
-                </Textbox>
-                <Button type="submit">Search</Button>
-            </form>
+            <SteamIDInputForm />
             {children}
         </div>
     );
