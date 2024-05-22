f1d488b8fc117f4336b9756380b4490e7ead1b75
suhankins <punishedbernadetta@niuitmo.ru>
2024-05-22 12:47:10 +0400

    removed auto scrolling
    it didn't work well in chrome

--- src/components/HallOfFame/HallOfFame.tsx
+++ src/components/HallOfFame/HallOfFame.tsx
@@ -1,12 +1,8 @@
-'use client';
-
 import { type ISteamStats } from '@/lib/ISteamStats';
 import HallOfFameBanner from './HallOfFameBanner';
 import { HallOfFameBannersDB } from '@/components/HallOfFame/HallOfFameBannersDB';
 import styles from './HallOfFame.module.css';
 import getKillsByWeaponType from '@/utils/getKills/getKillsByWeaponType';
-import { useMemo, useRef } from 'react';
-import useAutoScroller from './useAutoScroller';
 
 const getWeightedValue = (
     value: number,
@@ -15,30 +11,22 @@ const getWeightedValue = (
 ) => (typeof weight === 'number' ? weight * value : weight(value, totalKills));
 
 export default function HallOfFame({ stats }: { stats: ISteamStats }) {
-    const containerRef = useRef<HTMLDivElement>(null);
-    useAutoScroller(containerRef);
-    const totalKills = useMemo(
-        () => getKillsByWeaponType('all', stats),
-        [stats]
-    );
-    const cards = useMemo(
-        () =>
-            HallOfFameBannersDB.map((card) => ({
-                ...card,
-                value: card.getter(stats),
-            }))
-                .filter((card) => card.value > 0)
-                .toSorted(
-                    (a, b) =>
-                        getWeightedValue(a.value, a.weight, totalKills) -
-                        getWeightedValue(b.value, b.weight, totalKills)
-                )
-                .toReversed(),
-        [stats, totalKills]
-    );
+    const totalKills = getKillsByWeaponType('all', stats);
+    const cards = HallOfFameBannersDB.map((card) => ({
+        ...card,
+        value: card.getter(stats),
+    }))
+        .filter((card) => card.value > 0)
+        .toSorted(
+            (a, b) =>
+                getWeightedValue(a.value, a.weight, totalKills) -
+                getWeightedValue(b.value, b.weight, totalKills)
+        )
+        .toReversed();
+
     return (
         <>
-            <div className={styles.container} ref={containerRef}>
+            <div className={styles.container}>
                 {cards.map((banner, index) => (
                     <HallOfFameBanner
                         banner={banner}
