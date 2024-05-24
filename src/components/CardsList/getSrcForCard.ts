84a3c46dff2f75072bfc86896ed7697045cb5024
suhankins <punishedbernadetta@niuitmo.ru>
2024-05-25 01:52:44 +0400

    changed card image names to be card ids

--- src/components/CardsList/getSrcForCard.ts
+++ src/components/CardsList/getSrcForCard.ts
@@ -1,8 +1,8 @@
 import { type IInventoryCard } from '@/utils/steamAPI/getInventory';
 
 export default function getSrcForCard(card: IInventoryCard): string {
-    if ('texture' in card) {
-        return `/static/images/raid/cards/${card.texture}.png`;
+    if ('id' in card) {
+        return `/static/images/raid/cards/${card.id}.png`;
     }
     if (card.icon_url) {
         return `https://community.akamai.steamstatic.com/economy/image/${card.icon_url}/96fx96f?allow_animated=0`;
