faa190e809ed60d1395e8dfa7e919e088ad19cc0
suhankins <punishedbernadetta@niuitmo.ru>
2024-05-24 23:15:09 +0400

    added text if therd are no inventory items

--- src/components/CardsList/CardsList.tsx
+++ src/components/CardsList/CardsList.tsx
@@ -16,55 +16,63 @@ export default function CardsList({
     return (
         <>
             <h2>Challenge and booster cards</h2>
-            <div className={styles.container}>
-                <ul className={styles.cardsList}>
-                    {inventory.map((card, index) => (
-                        <li
-                            key={index}
-                            tabIndex={0}
-                            aria-label={card.name}
-                            aria-current={index === selectedCard}
-                            onClick={() => setSelectedCard(index)}
-                            onKeyDown={(e) => {
-                                if (e.key === ' ') {
-                                    e.preventDefault();
-                                    setSelectedCard(index);
-                                }
-                            }}
-                        >
-                            <Card card={card} />
-                        </li>
-                    ))}
-                </ul>
-                <div className={styles.cardDetails} aria-live="polite">
-                    <Card
-                        card={currentCard}
-                        size={2.5}
-                        onIconHoverShowTooltip
-                    />
-                    <div>
-                        <h3>{currentCard.name}</h3>
-                        {'positiveEffect' in currentCard && (
-                            <Effect>{currentCard.positiveEffect}</Effect>
-                        )}
-                        {'negativeEffect' in currentCard && (
-                            <Effect negative>
-                                {currentCard.negativeEffect}
-                            </Effect>
-                        )}
-                        {!(
-                            'positiveEffect' in currentCard ||
-                            'negativeEffect' in currentCard
-                        ) && (
-                            <div
-                                dangerouslySetInnerHTML={{
-                                    __html: currentCard.descriptions[0].value,
+            {inventory.length !== 0 ? (
+                <div className={styles.container}>
+                    <ul className={styles.cardsList}>
+                        {inventory.map((card, index) => (
+                            <li
+                                key={index}
+                                tabIndex={0}
+                                aria-label={card.name}
+                                aria-current={index === selectedCard}
+                                onClick={() => setSelectedCard(index)}
+                                onKeyDown={(e) => {
+                                    if (e.key === ' ') {
+                                        e.preventDefault();
+                                        setSelectedCard(index);
+                                    }
                                 }}
-                            />
-                        )}
+                            >
+                                <Card card={card} />
+                            </li>
+                        ))}
+                    </ul>
+                    <div className={styles.cardDetails} aria-live="polite">
+                        <Card
+                            card={currentCard}
+                            size={2.5}
+                            onIconHoverShowTooltip
+                        />
+                        <div>
+                            <h3>{currentCard.name}</h3>
+                            {'positiveEffect' in currentCard && (
+                                <Effect>{currentCard.positiveEffect}</Effect>
+                            )}
+                            {'negativeEffect' in currentCard && (
+                                <Effect negative>
+                                    {currentCard.negativeEffect}
+                                </Effect>
+                            )}
+                            {!(
+                                'positiveEffect' in currentCard ||
+                                'negativeEffect' in currentCard
+                            ) && (
+                                <div
+                                    dangerouslySetInnerHTML={{
+                                        __html: currentCard.descriptions[0]
+                                            .value,
+                                    }}
+                                />
+                            )}
+                        </div>
                     </div>
                 </div>
-            </div>
+            ) : (
+                <p>
+                    Our spies couldn't obtain any information about raider's
+                    cards!
+                </p>
+            )}
         </>
     );
 }
