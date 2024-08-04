3ed69fc012dd9a75f0bee3d829411862d83e9f91
suhankins <punishedbernadetta@niuitmo.ru>
2024-08-04 14:48:38 +0400

    updated cards

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'community.akamai.steamstatic.com',
+            },
+        ],
+    },
+};
 
 module.exports = nextConfig;
