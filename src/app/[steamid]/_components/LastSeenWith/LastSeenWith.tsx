ea597109382a147787244783ec66fb38fe89698e
suhankins <punishedbernadetta@niuitmo.ru>
2024-08-17 14:48:08 +0400

    last seen with block is no longer displayed if it's empty

--- src/app/[steamid]/_components/LastSeenWith/LastSeenWith.tsx
+++ src/app/[steamid]/_components/LastSeenWith/LastSeenWith.tsx
@@ -8,36 +8,29 @@ import { Weapon } from './Weapon';
 import { getWeaponById } from '@/utils/WeaponsDB';
 
 export default function LastSeenWith({ stats }: { stats: ISteamStats }) {
+    const primary = getWeaponById(getWeaponIdFromIndex(stats.equipped_primary));
+    const secondary = getWeaponById(
+        getWeaponIdFromIndex(stats.equipped_secondary)
+    );
+    const equipment = getWeaponById(
+        getGrenadeIdFromIndex(stats.equipped_grenade)
+    );
+    const melee = getWeaponById(getMeleeIdFromIndex(stats.equipped_melee));
+
+    if (!primary && !secondary && !equipment && !melee) {
+        return null;
+    }
+
     return (
         <>
             <h2 className={styles.header}>
                 Raider was last seen with these weapons
             </h2>
             <div className={styles.lastSeenWithGrid}>
-                <Weapon
-                    stats={stats}
-                    weapon={getWeaponById(
-                        getWeaponIdFromIndex(stats.equipped_primary)
-                    )}
-                />
-                <Weapon
-                    stats={stats}
-                    weapon={getWeaponById(
-                        getWeaponIdFromIndex(stats.equipped_secondary)
-                    )}
-                />
-                <Weapon
-                    stats={stats}
-                    weapon={getWeaponById(
-                        getGrenadeIdFromIndex(stats.equipped_grenade)
-                    )}
-                />
-                <Weapon
-                    stats={stats}
-                    weapon={getWeaponById(
-                        getMeleeIdFromIndex(stats.equipped_melee)
-                    )}
-                />
+                <Weapon stats={stats} weapon={primary} />
+                <Weapon stats={stats} weapon={secondary} />
+                <Weapon stats={stats} weapon={equipment} />
+                <Weapon stats={stats} weapon={melee} />
             </div>
         </>
     );
